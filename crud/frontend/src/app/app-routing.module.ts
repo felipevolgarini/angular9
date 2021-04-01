991a4905a3ab739177d4fd13c3fcb4efc9149568
Felipe Cesar Volgarini Madurro <felipe.madurro@comlink.com.br>
2021-04-01 13:40:32 -0300

    Atualizando produtos

--- crud/frontend/src/app/app-routing.module.ts
+++ crud/frontend/src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -18,7 +19,11 @@ const routes: Routes = [
   {
     path: "products/create",
     component: ProductCreateComponent
-  }    
+  },
+  {
+    path: "products/update/:id",
+    component: ProductUpdateComponent
+  }       
 ];
 
 @NgModule({
